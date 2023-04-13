9e0f97c1c8a1557879ef4e6ad924a73eaef200fa
vaRUN <58069627+NOTvaRUN@users.noreply.github.com>
2023-04-13 20:54:59 +0530

    table added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { catchError, map, startWith, switchMap } from 'rxjs';
+import {merge, Observable, of as observableOf} from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,84 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data_table';
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  displayedColumns: string[] = ['fav', 'code', 'name', 'state', 'country', 'domains', 'web_pages'];
+  isLoading = false;
+  value = '';
+  // dataSource: MatTableDataSource<any>;
+  dataSource: MatTableDataSource<any>;
+  @ViewChild(MatSort) sort: MatSort;
+  countries: any = {};
+  countriesArr: any = [];
+  resultsLength = 0;
+  selectedCountry = '';
+
+  constructor(public http: HttpClient){}
+
+  ngOnInit(){
+    this.getDataSource()
+  }
+
+  getDataSource(){
+    // merge(this.sort?.sortChange, this.paginator?.page).pipe(
+    //   startWith({}),
+    //   switchMap(()=>{
+    //     this.isLoading = true;
+    //     return this.http.get<any>('http://universities.hipolabs.com/search').pipe(catchError(() => observableOf(null)));
+    //   }),
+    //   map(data=>{
+    //     this.isLoading = false;
+    //     if(data === null){
+    //       return []
+    //     }
+
+    //     this.resultsLength = data.length;
+    //     return data
+    //   }
+    // )
+    // ).subscribe(data=>{
+    //   console.log(data);
+    // })
+
+
+    this.http.get<any>('http://universities.hipolabs.com/search').subscribe(resp=>{
+      this.dataSource = new MatTableDataSource(resp)
+      resp.map((data: any)=>{
+        this.countries[data.country] = data.country;
+        return data.country
+      })
+      this.countriesArr = Object.keys(this.countries)
+      this.dataSource.paginator = this.paginator;
+    })
+  }
+
+  setPagination(): void {
+    const paginator = setInterval(() => {
+      if (this.dataSource) {
+        clearInterval(paginator);
+        this.dataSource.paginator = this.paginator;
+      }
+    }, 100);
+  }
+
+  navigate(page: string){
+    window.open(page, "_blank")
+  }
+
+  applyFilter(event: Event){
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filterPredicate = function(data, filter: string): boolean {
+      return data.name.toLowerCase().includes(filter);
+    };
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  applyCountry(country: string){
+    console.log(country);
+    this.dataSource.filterPredicate = function(data, filter: string): boolean {
+      return data.country.toLowerCase().includes(filter);
+    };
+    this.dataSource.filter = country.trim().toLowerCase();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
+
+// IMPORTS 
+import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import { SearchPipe } from './pipes/search.pipe';
+import {MatSelectModule} from '@angular/material/select';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule,
+    MatCardModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatPaginatorModule,
+    MatBadgeModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
